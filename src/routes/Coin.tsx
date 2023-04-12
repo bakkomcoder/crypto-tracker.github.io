0a1a7fbe19c3eab9a3921cee18b17800d1c9c560
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-04-12 14:25:49 +0900

    update
    Coin Data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useLocation } from "react-router";
 import styled from "styled-components";
 
@@ -35,9 +35,27 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
-  // console.log(state.name); // Home에서 coin 클릭 후 넘어간다면 해당 코인의 이름(state.name)을 확인할 수 있다. -> 타이틀에 coin의 이름을 직접 뿌려줄 수 있다는 것!
-  // 이렇게 함으로써 Coin component에서 API를 부르지 않는다. 여기서 발생하는 문제가, home에서 coin을 누를 경우만 state를 전달 받아서 Coin 화면에 뿌려지는 건데
-  // 곧바로 Coin 화면으로 간다면 에러가 난다는 것. state가 정의되지 않았다. => 해결하기 위해 조건문 작성(null일 때 보여줄 화면 만들어줌)
+  // 가져온 데이터를 state에 넣어주자.
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      // const response =
+      //   await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}
+      // `);
+      // const json = await response.json(); // 이 두 줄을 아래의 한 줄로 캡슐화
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      // console.log(infoData);
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      // console.log(priceData); // Coin 화면에 필요한 데이터 준비 완료.
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
